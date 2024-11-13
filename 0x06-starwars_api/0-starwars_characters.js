a6269fd2ddaedae3ba5dd60bae01cc3ccd1c524c
Mbaruku2003 <leonardjembe21@gmail.com>
2024-11-13 12:47:59 +0300

    created a new file

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,30 +1,25 @@
 #!/usr/bin/node
 const request = require('request')
-const movieId = process.argv[2]
-if (!movieId) {
-	console.error("Please provide a movie ID")
-	process.exit(1);
-}
-const url = `https://swapi.dev/api/films/${movieID}/`;
-request(url, (error, response, body) => {
-	if (error) {
-		console.error(error);
-		return;
+const req = (arr, i) => {
+  if (i === arr.length) return;
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(JSON.parse(body).name);
+      req(arr, i + 1);
+    }
+  });
+};
 
-	}
-
-	if (response.statusCode === 200) {
-		const filmData = JSON.parse(body);
-		const characters = filmData.characters;
-		characters.forEach((characterUrl) => {
-			request(characterUrl, (error, response, body) => {
-				if (!error && response.statusCode === 200) {
-					const characterData = JSON.parse(body);
-					console.log(characterData.name);
-				}
-			});
-		});
-	} else {
-		console.error(`Failed to retreive movie Data for ${movieId}`);
-	}
-});
+request(
+  `https://swapi.api.hbtn.io/api/films/${process.argv[2]}`,
+  (err, response, body) => {
+    if (err) {
+      throw err;
+    }  else  {
+      const chars = json.parse(body).characters;
+	    req(chars, 0);
+    }
+  }
+);
